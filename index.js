c214309926f2e5b47edc3724eea784476fb08742
Qasim Abdullah <qasimabdullah404@gmail.com>
2022-05-20 21:26:55 +0500

    add cors

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { v4: uuidv4 } = require("uuid");
 const axios = require("axios");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const natters = {};
 
